7f293239a00b9f91781340bd42bedffe3c5e060c
James Pang <chihhong.pang@gmail.com>
2014-11-09 21:13:04 -0500

    -Javadoc for deposit, withdraw, transfer

--- Quinterac Back End/src/Deposit.java
+++ Quinterac Back End/src/Deposit.java
@@ -1,12 +1,33 @@
 import java.util.ArrayList;
 
-
+/**
+ * The Deposit class implements the required behavior for the "deposit" transaction.
+ * With its single method apart from the constructor, it adds a new line to the master
+ * accounts file based on the original account number and original name from the old
+ * master accounts file with the new amount after the deposit. Then it deletes the old entry
+ * from the old master accounts file.
+ * 
+ * @author James Pang
+ *
+ */
 public class Deposit extends Transaction{
-
+	
+	/**
+	 * This is the constructor for the Deposit class.
+	 * 
+	 * @param ma				the ArrayList of the master accounts file
+	 * @param transaction		the String of the command from the transaction summary file
+	 */
 	public Deposit(ArrayList<String> ma, String transaction) {
 		super(ma, transaction);
 	}
 	
+	/**
+	 * This method performs the simple task of calculating the new amount after the deposit.
+	 * It adds the new and updated entry first and remove the old entry.
+	 * The new entry contains the original account number, the new balance after the deposit,
+	 * and the original name.
+	 */
 	public void start() {
 		for (String account: masterAccounts) {
 			if (extractAccountFromAccountLine(account) == getToAccount()){
--- Quinterac Back End/src/Transfer.java
+++ Quinterac Back End/src/Transfer.java
@@ -1,12 +1,35 @@
 import java.util.ArrayList;
 
-
+/**
+ * The Transfer class implements the required behavior for the "transfer" transaction.
+ * Essentially it is a combination of deposit and withdraw transactions.
+ * With its single method apart from the constructor, it adds two new lines to the master
+ * accounts file based on the original account number and original name from the old
+ * master accounts file with the new amount after the transfer. Then it deletes the two old entries
+ * from the old master accounts file.
+ * 
+ * @author James Pang
+ *
+ */
 public class Transfer extends Transaction{
 
+	/**
+	 * This is the constructor for the Transfer class.
+	 * 
+	 * @param ma				the ArrayList of the master accounts file
+	 * @param transaction		the String of the command from the transaction summary file
+	 */
 	public Transfer(ArrayList<String> ma, String transaction) {
 		super(ma, transaction);
 	}
 
+	/**
+	 * This method performs the simple task of calculating the new amount after the transfer for both accounts.
+	 * If the new amount is negative, the program will generate an error message.
+	 * It adds the new and updated entry first and remove the old entry for both accounts.
+	 * The new entry contains the original account number, the new balance after the deposit,
+	 * and the original name.
+	 */
 	public static void start() {
 		for (String account: masterAccounts) {
 			if (extractAccountFromAccountLine(account) == getFromAccount()){
